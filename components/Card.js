5fa8c9eb8a2eb13c44ab6acfd046ecc272efc0fb
asep <aseprendi487@gmail.com>
2023-02-21 09:27:40 +0700

    Edit Card component

--- components/Card.js
+++ components/Card.js
@@ -1,56 +1,19 @@
 import Image from './Image'
 import Link from './Link'
 
-const Card = ({ title, description, imgSrc, href }) => (
-  <div className="md p-4 md:w-1/2" style={{ maxWidth: '544px' }}>
-    <div
-      className={`${
-        imgSrc && 'h-full'
-      }  overflow-hidden rounded-md border-2 border-gray-200 border-opacity-60 dark:border-gray-700`}
-    >
-      {imgSrc &&
-        (href ? (
-          <Link href={href} aria-label={`Link to ${title}`}>
-            <Image
-              alt={title}
-              src={imgSrc}
-              className="object-cover object-center md:h-36 lg:h-48"
-              width={544}
-              height={306}
-            />
-          </Link>
-        ) : (
-          <Image
-            alt={title}
-            src={imgSrc}
-            className="object-cover object-center md:h-36 lg:h-48"
-            width={544}
-            height={306}
-          />
-        ))}
-      <div className="p-6">
-        <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">
-          {href ? (
-            <Link href={href} aria-label={`Link to ${title}`}>
-              {title}
-            </Link>
-          ) : (
-            title
-          )}
-        </h2>
-        <p className="prose mb-3 max-w-none text-gray-500 dark:text-gray-400">{description}</p>
-        {href && (
-          <Link
-            href={href}
-            className="text-base font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-            aria-label={`Link to ${title}`}
-          >
-            Learn more &rarr;
-          </Link>
-        )}
+const Card = ({ title, imgSrc, href, description }) => (
+  <Link href={href} aria-label={`Link to ${title}`}>
+    <div className="w-60 mr-6 mt-6">
+      <div>
+        {/* <img className="w-60 h-36 rounded-2xl" src="static/images/projects/blogr.png" alt="blogr landing page" /> */}
+        <Image width={240} height={144} src={imgSrc} alt={title} className="rounded-2xl" />
+      </div>
+      <div className="pt-2 text-center">
+        <h3 className="text-gray-800 dark:text-white font-semibold">{title}</h3>
+        <p className="text-gray-600 dark:text-white/70">{description}</p>
       </div>
     </div>
-  </div>
+  </Link>
 )
 
 export default Card
