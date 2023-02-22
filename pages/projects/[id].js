6b3a05d902abde8324d53f4c205af6df38d3beaf
asep <aseprendi487@gmail.com>
2023-02-22 20:39:08 +0700

    Fix build error on vercel

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -1,34 +1,34 @@
 import Image from 'next/image'
-import { useRouter } from 'next/router'
+// import { useRouter } from 'next/router'
 import projectsData from '@/data/projectsData'
 
-export default function Project() {
-  const router = useRouter()
-  const { id } = router.query
-  const { title, description, url, images, role, stack } = projectsData[id - 1]
+export default function Project({ project }) {
+  // const router = useRouter()
+  // const { id } = router.query
+  // const { title, description, url, images, role, stack } = projectsData[id - 1]
 
   return (
     <section className="max-w-[550px] mx-auto ">
-      <h1 className="text-xl font-medium mb-2">{title}</h1>
-      <p className="indent-6">{description}</p>
+      <h1 className="text-xl font-medium mb-2">{project.title}</h1>
+      <p className="indent-6">{project.description}</p>
       <ul className="space-y-1 mt-6">
         <li>
           <span className="bg-gray-300 py-0.5 px-1 mr-2 rounded-sm">Stack</span>
-          {stack.join(' • ')}
+          {project.stack.join(' • ')}
         </li>
         <li>
           <span className="bg-gray-300 py-0.5 px-1 mr-2 rounded-sm">Website</span>
-          <a href={url} className="text-blue-500">
-            {url}
+          <a href={project.url} className="text-blue-500">
+            {project.url}
           </a>
         </li>
         <li>
           <span className="bg-gray-300 py-0.5 px-1 mr-2 rounded-sm">Role</span>
-          {role.join(' • ')}
+          {project.role.join(' • ')}
         </li>
       </ul>
       <div className="mt-4 space-y-4 flex flex-col justify-center items-center">
-        {images.map((image) => (
+        {project.images.map((image) => (
           <div key={image.id}>
             <Image width={500} height={320} src={image.imgSrc} alt={image.alt} />
           </div>
@@ -36,4 +36,17 @@ export default function Project() {
       </div>
     </section>
   )
+}
+
+export async function getStaticPaths() {
+  const paths = projectsData.map((project) => ({
+    params: { id: `${project.id}` },
+  }))
+
+  return { paths, fallback: false }
+}
+
+export async function getStaticProps({ params }) {
+  const project = projectsData[params.id - 1]
+  return { props: { project } }
 }
